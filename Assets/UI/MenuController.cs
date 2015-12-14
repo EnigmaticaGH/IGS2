4283799c5ac8c4b2ca66d97c1a87267829e125c6
Derek Shunia <eventhorizonsoundtracks@gmail.com>
2015-12-14 15:23:31 -0500

    Added a Main Menu

---
+++ Assets/UI/MenuController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayButtonClick()
+    {
+        Application.LoadLevel(1);
+    }
+}
