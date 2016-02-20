c9eb07cc3ff993856b07a4a2d639bb5960b474f0
Roman <Romanreiner9@gmail.com>
2016-02-20 01:59:50 -0500

    Hello It's me this is an update baby

---
+++ Assets/CharacterMenuButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharacterMenuButton : MonoBehaviour {
+
+    Button Button1;
+
+    void Start()
+    {
+        GetComponent<Button>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Cursor")
+        {
+            Debug.LogError("Button Collision Detected");
+            
+        }
+    }
+}
---
+++ Assets/CursorMovement_p3.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorMovement_p3 : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float CursorSpeed = 100;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("L_XAxis_");
+        float vertical = Input.GetAxis("L_YAxis_3");
+
+        rb.velocity = new Vector3(horizontal * CursorSpeed * Time.deltaTime, vertical * CursorSpeed * Time.deltaTime, 0);
+
+
+        // ...
+
+        // 6 - Make sure we are not outside the camera bounds
+        var dist = (transform.position - Camera.main.transform.position).z;
+
+        var leftBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).x;
+
+        var rightBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(1, 0, dist)
+        ).x;
+
+        var topBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 0, dist)
+        ).y;
+
+        var bottomBorder = Camera.main.ViewportToWorldPoint(
+          new Vector3(0, 1, dist)
+        ).y;
+
+        transform.position = new Vector3(
+          Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+          Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+          transform.position.z
+        );
+    }
+}
