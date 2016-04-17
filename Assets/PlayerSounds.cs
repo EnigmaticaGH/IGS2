f784dc0d15b18128fb3c26387cc218f27079861f
Roman <Romanreiner9@gmail.com>
2016-04-17 15:37:41 -0400

    sounds

---
+++ Assets/PlayerSounds.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSounds : MonoBehaviour 
+{
+
+    public AudioSource Source;
+
+    public AudioClip Walk;
+
+    public AudioClip Death;
+
+    public AudioClip CloudPop;
+
+    public AudioClip Punch; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Sound(string Action)
+    {
+
+    }
+}
